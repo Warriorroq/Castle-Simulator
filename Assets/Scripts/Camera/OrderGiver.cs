beccd7e7ba9ff4eaae6d2c094d83fc146e8731a4
Arthur <Warriorroq@gmail.com>
2021-10-04 20:33:34 +0300

    test for tasks

--- Assets/Scripts/Camera/OrderGiver.cs
+++ Assets/Scripts/Camera/OrderGiver.cs
@@ -1,18 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
+using UnitSpace;
+using UnitSpace.Orders;
 public class OrderGiver : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private TakeUnit _unitTaker;
+    [SerializeField] private Unit tackedUnit;
+    private void Awake()
     {
-        
+        _unitTaker = GetComponent<TakeUnit>();
+        _unitTaker.takeUnit.AddListener(TakeUnit);
     }
 
-    // Update is called once per frame
+    private void TakeUnit(Unit arg0)
+        => tackedUnit = arg0;
+
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(1))
+            AddMovePoint();
+    }
+    private void AddMovePoint()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out var hit, float.MaxValue))
+        {
+            tackedUnit?.unitOrders.AddOrder(new MoveToOrder(hit.point));
+        }
     }
 }
