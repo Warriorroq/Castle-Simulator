4b951191bb1afc687a6247e6c95381bb7fbcee72
Arthur <Warriorroq@gmail.com>
2021-10-10 21:21:50 +0300

    Alpha last parst (unnessesary)

--- Assets/Scripts/Camera/OrderGiver.cs
+++ Assets/Scripts/Camera/OrderGiver.cs
@@ -1,11 +1,28 @@
 using UnityEngine;
 using UnitSpace;
 using UnitSpace.Orders;
+using UnityEngine.Events;
+
 public class OrderGiver : MonoBehaviour
 {
+    public UnityEvent<Unit> unitTake;
     private TakeUnit _unitTaker;
     [SerializeField] private Unit _lastTakedUnit;
     [SerializeField] private Unit _takedUnit;
+    [SerializeField]private Unit _unitClone;
+
+    public void ClearUnitOrders()
+    {
+        _takedUnit?.unitOrders.ClearOrders();
+        _takedUnit?.unitOrders.StopOrder();
+    }
+    public void FollowUnit()
+        =>_takedUnit?.unitOrders.AddOrder(new FollowToOrder(_lastTakedUnit));
+    public void AttackUnit()
+    {
+        _takedUnit?.unitOrders.AddOrder(new FollowToOrder(_lastTakedUnit));
+        _takedUnit?.unitOrders.AddOrder(new AttackOrder(_lastTakedUnit));
+    }
     private void Awake()
     {
         _unitTaker = GetComponent<TakeUnit>();
@@ -14,51 +31,41 @@ public class OrderGiver : MonoBehaviour
 
     private void TakeUnit(Unit arg0)
     {
+        if (!_lastTakedUnit)
+            _lastTakedUnit = null;
+        if (!_takedUnit)
+            _takedUnit = null;
+        _lastTakedUnit?.unitSelector.ChangeSelectorColor(Color.white);
         if (_lastTakedUnit == arg0)
+        {
+            _takedUnit?.unitSelector.ChangeSelectorColor(Color.white);
             _takedUnit = _lastTakedUnit;
-        _lastTakedUnit = arg0;        
+            _takedUnit?.unitSelector.ChangeSelectorColor(Color.green);
+            unitTake.Invoke(_takedUnit);
+            _lastTakedUnit = null;
+            return;
+        }
+        _lastTakedUnit = arg0;
+        _lastTakedUnit?.unitSelector.ChangeSelectorColor(Color.red);
     }
 
     void Update()
     {
         if (Input.GetMouseButtonDown(1))
         {
-            if(!Input.GetKey(KeyCode.LeftControl))
-            {
-                //_takedUnit.unitOrders.ClearOrders();
-                //_takedUnit.unitOrders.StopOrder();
-            }
-            if (Input.GetKey(KeyCode.A))
-            {
-                if (_lastTakedUnit != _takedUnit)
-                    AttackUnit();
-            }
-            else if(Input.GetKey(KeyCode.F))
-            {
-                if (_lastTakedUnit != _takedUnit)
-                    FollowUnit();
-            }
-            else
-            {
-                AddMovePoint();
-            }
+            AddMovePoint();
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            CreateUnit();
         }
     }
-    private void FollowUnit()
+    private void CreateUnit()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out var hit, float.MaxValue))
         {
-            _takedUnit?.unitOrders.AddOrder(new FollowToOrder(_lastTakedUnit));
-        }
-    }
-    private void AttackUnit()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out var hit, float.MaxValue))
-        {
-            _takedUnit?.unitOrders.AddOrder(new FollowToOrder(_lastTakedUnit));
-            _takedUnit?.unitOrders.AddOrder(new AttackOrder(_lastTakedUnit));
+            Instantiate(_unitClone, hit.point + Vector3.up, Quaternion.identity);
         }
     }
     private void AddMovePoint()
