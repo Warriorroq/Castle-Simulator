579f42afca3994f894376d14983e3c5860022c2a
Arthur <Warriorroq@gmail.com>
2021-10-18 20:07:37 +0300

    Added Dila

---
+++ Assets/Scripts/Dila/ChangeSkyBoxes.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dila
+{
+    public class ChangeSkyBoxes : MonoBehaviour
+    {
+        [SerializeField] private float _skyBoxRotateSpeed;
+        public void ChangeSkyBox(Material material)
+            => RenderSettings.skybox = material;
+        private void Update()
+        {
+            RenderSettings.skybox.SetFloat("_Rotation", Time.time * _skyBoxRotateSpeed);
+        }
+    }
+}
---
+++ Assets/Scripts/Dila/Dilia.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Dila
+{
+    public class Dilia : MonoBehaviour
+    {
+        public UnityEvent startSaturn;
+        public UnityEvent<int> startMoon;
+        public UnityEvent endGame;
+        [SerializeField] private int _daysLeft;
+        [SerializeField] private float _timeForLap;
+        [SerializeField] private float _saturnTime;
+        void Start()
+        {
+            _saturnTime = _timeForLap * 3;
+            Invoke(nameof(OnSaturnTime), _saturnTime);
+        }
+        private void OnSaturnTime()
+        {
+            startSaturn.Invoke();
+            Invoke(nameof(OnMoonTime), _timeForLap);
+            if (_daysLeft <= 0)
+                endGame.Invoke();
+        }
+        private void OnMoonTime()
+        {
+            startMoon.Invoke(_daysLeft);
+            Invoke(nameof(OnSaturnTime), _saturnTime);
+            _daysLeft--;
+        }
+    }
+}
